a22c8fc61bec806212cfa20bb5375c83c4b22763
weikx <weikexin666@gmail.com>
2019-03-17 18:05:34 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path')
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev'
 function getHtmlConfig (name) {
 	return {
 		title: 'shit',
@@ -13,15 +15,16 @@ function getHtmlConfig (name) {
 	}
 }
 
-module.exports = {
+var config = {
 	entry: {
 		'common': ['./src/page/common/index.js'],
 		'index': ['./src/page/index/index.js'],
 		'login': ['./src/page/login/index.js']
 	},
 	output: {
-		filename: 'js/[name].js',
-		path: path.resolve(__dirname, 'dist')
+		path: './dist',
+		publicPath: '/dist',
+		filename: 'js/[name].js'
 	},
 	externals: {
 		'jquery': 'window.jQuery'
@@ -31,6 +34,10 @@ module.exports = {
 			{
 				test: /\.css$/,
 				loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+			},
+			{
+				test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,
+				loader: 'url-loader'
 			},
 			{
 				test: /\.(html)$/,
@@ -53,4 +60,10 @@ module.exports = {
 		new HtmlWebpackPlugin(getHtmlConfig('index')),
 		new HtmlWebpackPlugin(getHtmlConfig('login'))
 	]
+}
+
+module.exports = config
+
+if (WEBPACK_ENV === 'dev') {
+	config.entry.common.push('webpack-dev-server/client?http://localhost:8088/')
 }
