e98b94457c6b26f475c3ba04a5633b5e988b8612
weikx <weikexin666@gmail.com>
2019-03-20 18:02:05 +0800

    完善登录页面

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,2 +1,70 @@
-console.log('shit login')
-require('../module.js')
+require('./index.css')
+require('page/common/footer/index.js')
+require('page/common/nav-simple/index.js')
+var _ace = require('util/ace.js')
+
+// 表单错误提示
+var fromError = {
+	show: function (msg) {
+		$('.err-box').show().find('.err-msg').text(msg)
+	},
+	hide: function () {
+		$('.err-box').hide().find('.err-msg').text('')
+	}
+}
+
+var page = {
+	init: function () {
+		this.bindEvent()
+	},
+	bindEvent: function () {
+		var _this = this
+		$('#submit').click(function () {
+			_this.submit()
+		})
+		$('.user-content').keyup(function (e) {
+			// 按下回车
+			if (e.keyCode == 13) {
+				_this.submit()
+			}
+		})
+	},
+	// 表单提交
+	submit: function () {
+		var formData = {
+			username: $.trim($('#username').val()),
+			password: $.trim($('#password').val())
+		}
+		// 表单验证结果
+		var validateResult = this.formValidate(formData)
+		if (validateResult.status) {
+			// 登录
+			fromError.hide()
+			console.log('登录')
+		} else {
+			fromError.show(validateResult.msg)
+		}
+	},
+	// 表单字段验证
+	formValidate: function (formData) {
+		var result = {
+			status  : false,
+			msg     : ''
+		}
+		if (!_ace.validate(formData.username, 'require')) {
+			result.msg = '请输入用户名'
+			return result
+		}
+		if (!_ace.validate(formData.password, 'require')) {
+			result.msg = '请输入密码'
+			return result
+		}
+		result.status = true
+		result.msg = 'nice'
+		return result
+	}
+}
+
+$(function () {
+	page.init()
+})
