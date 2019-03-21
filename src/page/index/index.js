07766789075abb62e756f685cee1234607c34ed1
weikx <weikexin666@gmail.com>
2019-03-21 18:38:17 +0800

    增加显示侧边栏

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,3 +1,7 @@
-var _ace = require('util/ace.js')
-require('page/common/nav-simple/index.js')
 require('page/common/footer/index.js')
+require('page/common/nav-simple/index.js')
+var _ace = require('util/ace.js')
+var navSide = require('page/common/nav-side/index.js')
+navSide.init({
+	name: 'order-list'
+})
