99f0a48c9a389d9e258af9430116bf9b148ae3ed
weikx <weikexin666@gmail.com>
2019-03-21 19:19:56 +0800

    init

---
+++ src/page/my-ace/index.js
@@ -0,0 +1,8 @@
+require('./index.css')
+require('page/common/footer/index.js')
+require('page/common/nav-simple/index.js')
+var _ace = require('util/ace.js')
+var navSide = require('page/common/nav-side/index.js')
+navSide.init({
+	name: 'my-ace'
+})
