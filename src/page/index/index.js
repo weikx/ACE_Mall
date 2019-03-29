40eb1be64f6aab154f9dd08f427ddd8a4170ac58
weikx <weikexin666@gmail.com>
2019-03-29 22:02:34 +0800

    修改样式

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,11 +1,12 @@
 require('./index.css')
-require('page/common/footer/index.js')
-require('page/common/nav-simple/index.js')
 require('page/common/nav/index.js')
+require('page/common/footer/index.js')
+require('../common/goods-item/index.js')
+require('page/common/nav-simple/index.js')
 var _ace = require('util/ace.js')
-var navList = require('page/common/nav/index.js')
 var swiperTemplate = require('./swiper.ace')
-var goodsTemplate = require('./goods-item.ace')
+var navList = require('page/common/nav/index.js')
+var goodsTemplate = require('../common/goods-item/index.ace')
 
 navList.init({
 	name: 'home'
