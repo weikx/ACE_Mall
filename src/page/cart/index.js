0ad4b766f07429feb88acde8a765dec5dd67792f
weikx <weikexin666@gmail.com>
2019-03-30 22:58:27 +0800

    增加购物车

---
+++ src/page/cart/index.js
@@ -0,0 +1,10 @@
+require('./index.css')
+require('page/common/footer/index.js')
+require('page/common/nav-simple/index.js')
+require('page/common/nav/index.js')
+var _ace = require('util/ace.js')
+var navList = require('page/common/nav/index.js')
+
+navList.init({
+	name: ''
+})
