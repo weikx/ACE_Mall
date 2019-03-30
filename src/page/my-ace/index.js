9effb51f2165db7bb94a6914651b57e0ca25a630
weikx <weikexin666@gmail.com>
2019-03-30 23:01:21 +0800

    update

--- src/page/my-ace/index.js
+++ src/page/my-ace/index.js
@@ -1,9 +1,12 @@
 require('./index.css')
+require('page/common/nav/index.js')
 require('page/common/footer/index.js')
 require('page/common/nav-simple/index.js')
 var _ace = require('util/ace.js')
+var navList = require('page/common/nav/index.js')
 var navSide = require('page/common/nav-side/index.js')
 
 navSide.init({
 	name: 'my-ace'
 })
+navList.init()
