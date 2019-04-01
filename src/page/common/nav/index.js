a59b070e2313a4b5d91c81c2e92e1b41bdd3a7d1
weikx <weikexin666@gmail.com>
2019-04-01 23:10:23 +0800

    接口对接

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,40 +1,16 @@
 require('./index.css')
 var _ace = require('util/ace.js')
 var templateIndex = require('./index.ace')
+var _goods = require('service/goods-service.js')
 // 导航
 var navList = {
 	option: {
-		name: '',
+		categoryId: '',
 		navList: [
 			{
-				name: 'home',
-				desc: '首页',
+				categoryId: 'home',
+				categoryName: '首页',
 				href: './index.html'
-			},
-			{
-				name: 'all',
-				desc: '所有产品',
-				href: './goods-list.html'
-			},
-			{
-				name: 'fearless',
-				desc: '无畏系列',
-				href: './goods-list.html'
-			},
-			{
-				name: 'logo',
-				desc: 'A.C.E.LOGO系列',
-				href: './goods-list.html'
-			},
-			{
-				name: 'cuban',
-				desc: 'CUBAN系列',
-				href: './goods-list.html'
-			},
-			{
-				name: 'tennis',
-				desc: 'TENNIS系列',
-				href: './goods-list.html'
 			}
 		]
 	},
@@ -42,14 +18,14 @@ var navList = {
 	init: function (option) {
 		// 合并选项
 		$.extend(this.option, option)
-		this.renderNav()
+		this.getGoodsCategory()
 	},
 	// 渲染导航菜单
 	renderNav: function () {
 		// 计算 active 数据
 		var _this = this
 		this.option.navList.forEach(function (item, index) {
-			if (item.name === _this.option.name) {
+			if (item.categoryId == _this.option.categoryId) {
 				item.isActive = true
 			}
 		})
@@ -59,6 +35,17 @@ var navList = {
 		})
 		// 把html放入容器
 		$('.nav-wrap .nav-list').html(navHtml)
+	},
+
+	getGoodsCategory: function () {
+		var _this = this
+		_goods.getGoodsCategory(function (res) {
+			res.forEach(function (item) {
+				item.href = './goods-list.html'
+				navList.option.navList.push(item)
+			})
+			_this.renderNav()
+		})
 	}
 }
 
