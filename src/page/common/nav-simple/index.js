e5c0ddcb385b8f8be4eb65c4dac30500ef0346b2
weikx <weikexin666@gmail.com>
2019-04-04 23:50:08 +0800

    增加登录状态

--- src/page/common/nav-simple/index.js
+++ src/page/common/nav-simple/index.js
@@ -1,1 +1,53 @@
 require('./index.css')
+var _ace = require('util/ace.js')
+var _user = require('service/user-service.js')
+
+var navSimple = {
+	data: {
+		userInfo: _ace.getUserInfo.info()
+	},
+
+	init: function () {
+		this.onLoad()
+	},
+
+	onLoad: function () {
+		this.setUserData()
+		this.setCartCount()
+		this.bindEvent()
+	},
+
+	bindEvent: function () {
+		var _this = this
+		$('.logout').click(function () {
+			_this.logout()
+		})
+		$('.js-login').click(function () {
+			_this.login()
+		})
+	},
+
+	setUserData: function () {
+		navSimple.data.userInfo && $('.no-login').hide()
+			.siblings('.login').show()
+			.find('.username').text(navSimple.data.userInfo.receiveName)
+	},
+
+	setCartCount: function () {
+		// 设置购物车数量
+		navSimple.data.userInfo && _user.getShopCart(function (res) {
+			$('.cart-count').text(res.length)
+		})
+	},
+
+	logout: function () {
+		_ace.logout()
+		window.location.reload()
+	},
+
+	login: function () {
+		_ace.toLogin()
+	}
+}
+
+navSimple.init()
