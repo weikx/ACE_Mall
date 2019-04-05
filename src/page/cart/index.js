a6205877ec1f1657d81b09a12d11e0865d420480
weikx <weikexin666@gmail.com>
2019-04-05 19:25:37 +0800

    update

--- src/page/cart/index.js
+++ src/page/cart/index.js
@@ -4,7 +4,68 @@ require('page/common/nav-simple/index.js')
 require('page/common/nav/index.js')
 var _ace = require('util/ace.js')
 var navList = require('page/common/nav/index.js')
+var cartTemplate = require('page/cart/index.ace')
+var _user = require('service/user-service.js')
 
 navList.init({
 	categoryId: ''
+})
+
+var cart = {
+	data: {
+		goods: []
+	},
+
+	init: function () {
+		this.onLoad()
+	},
+
+	onLoad: function () {
+		this.bindEvent()
+		this.getShopCart()
+	},
+
+	bindEvent: function () {
+		$(document).on('click', '.goods-count-btn', function () {
+			var $this = $(this),
+				goodsId = $this.parents('.list-item-wrap').data('id'),
+				type = $this.hasClass('plus') ? 'plus' : 'minus',
+				$goodsCount = $this.siblings('.goods-count'),
+				currCount = parseInt($goodsCount.val()),
+				maxCount = $goodsCount.data('stock'),
+				minCount = 1
+			if (type === 'plus') {
+				$goodsCount.val(currCount > maxCount ? maxCount : currCount + 1)
+			} else {
+				$goodsCount.val(currCount < minCount ? minCount : currCount - 1)
+			}
+			_user.updateShopCartNum({
+				userId: _ace.getUserInfo.id(),
+				goodId: goodsId,
+				number: currCount
+			})
+		})
+	},
+
+	getShopCart: function () {
+		_user.getShopCart(function (res) {
+			cart.data.goods = res
+			var cartHtml = _ace.renderHtml(cartTemplate, cart.data)
+			$('.cart-goods-list').html(cartHtml)
+		})
+	},
+
+	updateShopCartNum: function () {
+		_user.updateShopCartNum({
+			userId: 0,
+			goodId: 0,
+			number: 0
+		}, function (res) {
+			console.log(res)
+		})
+	}
+}
+
+$(function () {
+	cart.init()
 })
