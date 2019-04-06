f1202a594db7f8f67dfd856be08be5ceec70d7de
weikx <weikexin666@gmail.com>
2019-04-06 23:34:20 +0800

    增加接口

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -50,6 +50,48 @@ var _user = {
 			success: success,
 			error: error
 		})
+	},
+
+	setGoodsChecked: function (params, success, error) {
+		_ace.request({
+			url: _ace.getServerUrl('api/User/SetGoodsChecked'),
+			method: 'POST',
+			data: params,
+			success: success,
+			error: error
+		})
+	},
+
+	setGoodsAllChecked: function (params, success, error) {
+		_ace.request({
+			url: _ace.getServerUrl('api/User/SetGoodsAllChecked'),
+			method: 'POST',
+			data: params,
+			success: success,
+			error: error
+		})
+	},
+
+	deleteGoods: function (params, success, error) {
+		_ace.request({
+			url: _ace.getServerUrl('api/User/DeleteShopCartGood'),
+			method: 'DELETE',
+			data: params,
+			success: success,
+			error: error
+		})
+	},
+
+	postOrderShow: function (params, success, error) {
+		_ace.request({
+			url: _ace.getServerUrl('api/User/PostOrderShow'),
+			method: 'POST',
+			data: {
+				userId: _ace.getUserInfo.id()
+			},
+			success: success,
+			error: error
+		})
 	}
 }
 
