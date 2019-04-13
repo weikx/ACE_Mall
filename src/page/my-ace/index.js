4a9d906ac1a71e683ed135a785065e9aa037fa50
weikx <weikexin666@gmail.com>
2019-04-13 18:37:56 +0800

    update

--- src/page/my-ace/index.js
+++ src/page/my-ace/index.js
@@ -27,13 +27,106 @@ var page = {
   },
 
 	bindEvent: function () {
+		var val = '',
+				_this = this
 
+		$('.link.change').on('click', function () {
+			var type = $(this).data('type')
+      val = $('.' + type).val()
+      $('.' + type).removeAttr('disabled').focus()
+    })
+		
+		$('.input').blur(function () {
+			_this.setData($(this), val)
+    }).keydown(function (e) {
+			if (e.keyCode === 13) {
+        $(this).attr('disabled', true)
+			}
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+    // var userName = '',
+			// 	phoneNum = '',
+			// 	nameNode = $('.user-name'),
+			// 	phoneNode = $('.user-phone')
+    // $('#change-name').on('click', function () {
+			// // 修改用户名点击
+			// userName = nameNode.text()
+			// nameNode.attr('contenteditable', true).
+			// addClass('focus').focus()
+    // })
+    //
+    //
+    // $('#change-phone').on('click', function () {
+			// // 修改手机点击
+    //   phoneNum = phoneNode.val()
+    //   phoneNode.removeAttr('disabled').
+    //   addClass('focus').focus()
+    // })
+    //
+    // nameNode.blur(function () {
+    // 	// 用户名失焦
+    // 	if (!nameNode.text()) {
+    // 		nameNode.text(userName)
+			// }
+    //   nameNode.attr('contenteditable', false).
+			// removeClass('focus')
+    // }).keydown(function (e) {
+    // 	if (e.keyCode === 13) {
+    //     nameNode.attr('contenteditable', false)
+    //   }
+    // })
+    //
+    // phoneNode.blur(function () {
+    // 	// 手机号失焦
+    //   if (!_ace.validate(phoneNode.val(), 'phone')) {
+    //   	_ace.errorTips('请输入正确的手机号')
+    //     phoneNode.val(phoneNum)
+    //   }
+    //   phoneNode.attr('disabled', true).
+    //   removeClass('focus')
+    // }).keydown(function (e) {
+    //   if (e.keyCode === 13) {
+    //     phoneNode.attr('disabled', true)
+    //   }
+    // })
   },
 
 	getMyData: function () {
+		// 获取个人信息
 		_user.getMyData(function (res) {
 			page.data.userData = res[0]
     })
+  },
+	
+	changeName: function () {
+		$('#change-name')
+  },
+
+	setData: function ($this, val) {
+    var inputClass = $this.eq(0).attr('class').split(' ')[1],
+				type = inputClass,
+        inputNode = $('.' + inputClass),
+				thisValue = inputNode.val()
+    inputNode.attr('disabled', true)
+    if (!thisValue) {
+      _ace.errorTips('请输入正确内容')
+      inputNode.val(val)
+    }
+
+    if (type === 'phone' && !_ace.validate(thisValue, 'phone')) {
+    	_ace.errorTips('请输入正确的手机号')
+		}
   }
 }
 
