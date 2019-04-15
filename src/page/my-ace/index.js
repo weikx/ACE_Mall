901f40694658af77dbc23f5cab943bf7eb203bbd
weikx <weikexin666@gmail.com>
2019-04-15 19:00:46 +0800

    完善个人中心

--- src/page/my-ace/index.js
+++ src/page/my-ace/index.js
@@ -6,6 +6,7 @@ var _ace = require('util/ace.js')
 var navList = require('page/common/nav/index.js')
 var navSide = require('page/common/nav-side/index.js')
 var _user = require('service/user-service.js')
+var pageTemplate = require('./index.ace')
 
 navSide.init({
 	name: 'my-ace'
@@ -24,88 +25,51 @@ var page = {
 	onLoad: function () {
 		this.bindEvent()
 		this.getMyData()
+    this.sayHello()
+
   },
 
 	bindEvent: function () {
 		var val = '',
 				_this = this
 
-		$('.link.change').on('click', function () {
-			var type = $(this).data('type')
+    $(document).on('click', '.link.change', function () {
+      // 点击修改按钮 使 input 可输入
+      var type = $(this).data('type')
       val = $('.' + type).val()
       $('.' + type).removeAttr('disabled').focus()
     })
-		
-		$('.input').blur(function () {
-			_this.setData($(this), val)
-    }).keydown(function (e) {
-			if (e.keyCode === 13) {
-        $(this).attr('disabled', true)
-			}
+    $(document).on('blur', '.input', function () {
+      // input 失焦上传信息
+      _this.setData($(this), val)
     })
-
-
-
-
-
-
-
-
-
-
-
-
-    // var userName = '',
-			// 	phoneNum = '',
-			// 	nameNode = $('.user-name'),
-			// 	phoneNode = $('.user-phone')
-    // $('#change-name').on('click', function () {
-			// // 修改用户名点击
-			// userName = nameNode.text()
-			// nameNode.attr('contenteditable', true).
-			// addClass('focus').focus()
-    // })
-    //
-    //
-    // $('#change-phone').on('click', function () {
-			// // 修改手机点击
-    //   phoneNum = phoneNode.val()
-    //   phoneNode.removeAttr('disabled').
-    //   addClass('focus').focus()
-    // })
-    //
-    // nameNode.blur(function () {
-    // 	// 用户名失焦
-    // 	if (!nameNode.text()) {
-    // 		nameNode.text(userName)
-			// }
-    //   nameNode.attr('contenteditable', false).
-			// removeClass('focus')
-    // }).keydown(function (e) {
-    // 	if (e.keyCode === 13) {
-    //     nameNode.attr('contenteditable', false)
-    //   }
-    // })
-    //
-    // phoneNode.blur(function () {
-    // 	// 手机号失焦
-    //   if (!_ace.validate(phoneNode.val(), 'phone')) {
-    //   	_ace.errorTips('请输入正确的手机号')
-    //     phoneNode.val(phoneNum)
-    //   }
-    //   phoneNode.attr('disabled', true).
-    //   removeClass('focus')
-    // }).keydown(function (e) {
-    //   if (e.keyCode === 13) {
-    //     phoneNode.attr('disabled', true)
-    //   }
-    // })
+    $(document).on('keydown', '.input', function (e) {
+      if (e.keyCode === 13) {
+        $(this).attr('disabled', true)
+      }
+    })
+    $(document).on('change', '#file', function (e) {
+      // 修改头像
+      _this.getUrlBase64(this, function (img) {
+        _user.uploadImg({
+          file: img
+        }, function (res) {
+          $('#avater-img').attr('src', res.img)
+          _this.updateMyData()
+        })
+      })
+    })
   },
 
 	getMyData: function () {
 		// 获取个人信息
+    var _this = this
 		_user.getMyData(function (res) {
-			page.data.userData = res[0]
+		  if (res) {
+        res[0].sayHello = _this.sayHello()
+      }
+      page.data.userData = res[0]
+      _this.renderPage(res[0])
     })
   },
 	
@@ -117,16 +81,83 @@ var page = {
     var inputClass = $this.eq(0).attr('class').split(' ')[1],
 				type = inputClass,
         inputNode = $('.' + inputClass),
-				thisValue = inputNode.val()
+				thisValue = inputNode.val(),
+        _this = this
     inputNode.attr('disabled', true)
     if (!thisValue) {
       _ace.errorTips('请输入正确内容')
       inputNode.val(val)
+    } else if (type === 'phone' && !_ace.validate(thisValue, 'phone')) {
+      _ace.errorTips('请输入正确的手机号')
+      inputNode.val(val)
+    } else if (type === 'email' && !_ace.validate(thisValue, 'email')) {
+      _ace.errorTips('请输入正确的邮箱')
+      inputNode.val(val)
+    } else if (thisValue !== val) {
+      if (type == 'account') {
+        $('.user-info .account-name').text(thisValue)
+      }
+      _this.updateMyData()
     }
+  },
 
-    if (type === 'phone' && !_ace.validate(thisValue, 'phone')) {
-    	_ace.errorTips('请输入正确的手机号')
-		}
+  getInputVal (type) {
+	  return $('.input.' + type).val()
+  },
+
+  updateMyData () {
+	  var _this = this
+    _user.updateMyData({
+      userId: _ace.getUserInfo.id(),
+      email: _this.getInputVal('email'),
+      image: $('#avater-img').attr('src'),
+      password: _this.getInputVal('password'),
+      receiveName: _this.getInputVal('name'),
+      receivePhone: _this.getInputVal('phone'),
+      receiveAddress: _this.getInputVal('address')
+    })
+  },
+
+  renderPage: function (data) {
+    var pageHtml = _ace.renderHtml(pageTemplate, data)
+    $('.content').html(pageHtml)
+  },
+
+  sayHello: function () {
+    var date = new Date().getHours()
+    if (date >= 22 && date < 5) {
+      return '该睡觉了'
+    } else if (date >= 5 && date < 8) {
+      return '早晨好'
+    } else if (date >= 8 && date < 11) {
+      return '上午好'
+    } else if (date >= 11 && date < 13) {
+      return '中午好'
+    } else if (date >= 13 && date < 18) {
+      return '下午好'
+    } else if (date >= 18 && date < 22) {
+      return '晚上好'
+    } else {
+      return '你好'
+    }
+  },
+
+  getUrlBase64: function ($file, callback) {
+	  // 图片转 Base64
+    var src = window.URL.createObjectURL($file.files[0]),
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        img = new Image
+    img.crossOrigin = 'Anonymous'
+    img.src = src
+    img.onload = function () {
+      canvas.height = img.height
+      canvas.width = img.width
+      ctx.drawImage(img, 0, 0, img.width, img.height)
+      var dataURL = canvas.toDataURL('image/jpg')
+      callback.call(this, dataURL.slice(22))
+      canvas = null
+    }
   }
 }
 
