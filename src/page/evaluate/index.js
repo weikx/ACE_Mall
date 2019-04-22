543924e48e2d5d375253abcdd527ba4bbab042d7
weikx <weikexin666@gmail.com>
2019-04-22 07:27:42 -0700

    增加页面

---
+++ src/page/evaluate/index.js
@@ -0,0 +1,8 @@
+require('./index.css')
+require('page/common/footer/index.js')
+require('page/common/nav-simple/index.js')
+require('page/common/nav/index.js')
+var _ace = require('util/ace.js')
+var navList = require('page/common/nav/index.js')
+var _goods = require('service/goods-service.js')
+navList.init()
